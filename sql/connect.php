e2d9915a195163a2f2627dc1413d2932f5e1c3c5
ichang97 <wprakongchai@gmail.com>
2021-01-25 19:32:55 +0700

    Update connect.php

--- sql/connect.php
+++ sql/connect.php
@@ -1,5 +1,5 @@
 <?php
-$con = mysqli_connect("localhost","dekcomch_employee","Chang5610160**","dekcomch_employee") or die("Error: " . mysqli_error($con));
+$con = mysqli_connect("localhost","xxxxx","xxxxx","xxxx") or die("Error: " . mysqli_error($con));
 
 mysqli_query($con,"SET NAMES 'utf8' ");
 
